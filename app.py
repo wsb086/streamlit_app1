467e54809c06bb1a4396589aa3893458efd4d8e1
wsb086 <156738767+wsb086@users.noreply.github.com>
2024-03-25 13:44:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import json
-
+import re
 # Streamlit 页面设置
 st.title('搜索服务')
 st.write('输入查询，获取相关文本。')
@@ -10,36 +10,36 @@ st.write('输入查询，获取相关文本。')
 API_URL = 'http://test18.kydev.net/rag/search_string'
 
 # 文本类型字典
-c_dict={
-'01':'二级概述',
-'02':'三级_症状_概述_1',
-'03':'三级_优先检查_概述_1',
-'04':'三级_诊断标准_概述_1',
-'05':'三级_查体与体征_概述_1',
-'06':'三级_问诊与症状_概述_1',
-'07':'三级_体征_概述_1',
-'08':'三级_查体技巧_概述_1',
-'09':'三级_问诊技巧_概述_1',
-'10':'三级_新检查_概述_1',
-'11':'三级_风险评估和危险分层_概述_1',
-'12':'三级_可选检查_概述_1',
-'13':'三级_危险因素_概述_1',
-'14':'三级_诱因_概述_1',
-'15':'三级_症状_概述_0',
-'16':'三级_优先检查_概述_0',
-'17':'三级_诊断标准_概述_0',
-'18':'三级_查体与体征_概述_0',
-'19':'三级_问诊与症状_概述_0',
-'20':'三级_体征_概述_0',
-'21':'三级_查体技巧_概述_0',
-'22':'三级_问诊技巧_概述_0',
-'23':'三级_新检查_概述_0',
-'24':'三级_风险评估和危险分层_概述_0',
-'25':'三级_可选检查_概述_0',
-'26':'三级_危险因素_概述_0',
-'27':'三级_诱因_概述_0',
-'28':'三级内容'
-}
+# c_dict={
+# '01':'二级概述',
+# '02':'三级_症状_概述_1',
+# '03':'三级_优先检查_概述_1',
+# '04':'三级_诊断标准_概述_1',
+# '05':'三级_查体与体征_概述_1',
+# '06':'三级_问诊与症状_概述_1',
+# '07':'三级_体征_概述_1',
+# '08':'三级_查体技巧_概述_1',
+# '09':'三级_问诊技巧_概述_1',
+# '10':'三级_新检查_概述_1',
+# '11':'三级_风险评估和危险分层_概述_1',
+# '12':'三级_可选检查_概述_1',
+# '13':'三级_危险因素_概述_1',
+# '14':'三级_诱因_概述_1',
+# '15':'三级_症状_概述_0',
+# '16':'三级_优先检查_概述_0',
+# '17':'三级_诊断标准_概述_0',
+# '18':'三级_查体与体征_概述_0',
+# '19':'三级_问诊与症状_概述_0',
+# '20':'三级_体征_概述_0',
+# '21':'三级_查体技巧_概述_0',
+# '22':'三级_问诊技巧_概述_0',
+# '23':'三级_新检查_概述_0',
+# '24':'三级_风险评估和危险分层_概述_0',
+# '25':'三级_可选检查_概述_0',
+# '26':'三级_危险因素_概述_0',
+# '27':'三级_诱因_概述_0',
+# '28':'三级内容'
+# }
 
 # 加载ID和文本的映射
 @st.cache
@@ -56,25 +56,50 @@ def get_text_type(text_id):
     return c_dict.get(type_code, "未知类型")  # 从字典中查找对应的文本类型
 
 # 创建一个文本输入框
+c_dict = {
+    '词条名': '99',
+    '一级': '98',
+    '二级': '97',
+    '二级概述': '01',
+    '三级_x_概述_1': ['02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14'],
+    '三级_x_概述_0': ['15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27'],
+    '三级内容': '28'
+}
+def standardize_category1(category):
+    return re.sub(r'三级_(.+?)_概述_1', r'三级_x_概述_1', category)
+def standardize_category2(category):
+    return re.sub(r'三级_(.+?)_概述_0', r'三级_x_概述_0', category)
 query = st.text_input('请输入你的查询：')
 number = st.number_input('请输入召回数目(1-50)', min_value=1, max_value=50, value=20)
+
+# 创建复选框让用户选择展示哪些列
+options = list(c_dict.keys())
+selected_options = st.multiselect('选择展示的类别：', options)
+
 # 当用户输入查询并点击按钮时
 if st.button('搜索'):
     if query:  # 确保输入不为空
         # 发送请求到接口
-        
-        response = requests.post(API_URL, json={"query": query,'n':number})
+        response = requests.post(API_URL, json={"query": query, 'n': number})
         if response.status_code == 200:
-            # 解析返回的ID列表
+            # 解析返回的数据列表
             top_ids = response.json().get('data', [])
             top_sim = response.json().get('sim', [])
             column = response.json().get('col_name', [])
             content = response.json().get('content', [])
-            # 获取对应的文本和类型
-            #results = [(id_to_text.get(top_ids[i], "未找到对应的文本"), get_text_type(top_ids[i]),top_sim[i]) for i in range(len(top_ids))]
-            # 将结果显示在网页上
-            results=[(top_ids[i],column[i],top_sim[i],content[i]) for i in range(len(top_ids))]
-            for id,col1,sim,con in results:
+
+            # 将结果按照用户选择的类别筛选
+            filtered_results = []
+            for i in range(len(top_ids)):
+                col1 = column[i]
+                standardized = standardize_category2(standardize_category1(col1))
+                # 检查当前列是否在用户选择的选项中
+                if standardized in selected_options:
+                    filtered_results.append((top_ids[i], col1, top_sim[i], content[i]))
+                        
+
+            # 将筛选后的结果显示在网页上
+            for id, col1, sim, con in filtered_results:
                 st.write(f"ID: {id}")
                 st.write(f"类型: {col1}")
                 st.write(f"相似度:{sim}")
